562a1726a64ad1f7dcd7f4a5af6a7f8dad12dd1f
dinaamlll <dinamaliaa17@gmail.com>
2024-06-09 00:05:55 +0700

    react.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" 
+          className="App-logo" 
+          alt="JavaScript logo" 
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the JavaScript World! 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.javascript.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn JavaScript More
         </a>
       </header>
     </div>
